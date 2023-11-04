e95f49d0d1a15c9f811877dc1388d86a32b181d4
lynnndengz <zden9056@uni.sydney.edu.au>
2023-11-04 20:38:06 +1100

    add comments

--- sketch.js
+++ sketch.js
@@ -1,40 +1,53 @@
+// Create an array to store circles
 let circles = [];
+// maximum radius for circles
 const maxRadius = 40;
+// radius for the large central circles
 const bigCircleRadius = 80;
+//use framecount to control time
 let frameCount = 0;
-let smallCirclesAnimated = false; // Flag to track if small circles animation has started
-let scaleUp = true; // Flag to track whether to scale up or down
+// track if small circles animation has started
+let smallCirclesAnimated = false;
+//  track whether to scale up or down
+let scaleUp = true;
+// Interval for toggling the scale direction
 let scaleInterval;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
+  // Create the initial set of circles
   makeCircles();
 
+  // Toggle the scale direction every 3 seconds
   scaleInterval = setInterval(function () {
     scaleUp = !scaleUp;
   }, 3000);
 }
 
 function makeCircles() {
+  // Clear the circles array
   circles = [];
+  // Calculate the number of columns and rows for the central circles
   let cols = floor(width / (bigCircleRadius * 2 + 5));
   let rows = floor(height / (bigCircleRadius * 2 + 5));
-
+  // Create a grid of circles with variations
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
+      // Position for each circle
       let x = (bigCircleRadius + 2.5) + col * (bigCircleRadius * 2 + 5);
       let y = (bigCircleRadius + 2.5) + row * (bigCircleRadius * 2 + 5);
+      // Generate a random hue for the central circle
       let hue = random(360);
       circles.push(new BigCircle(x, y, bigCircleRadius, color(hue, 5, 90)));
-
+      // Create three small circles around the central one
       for (let j = 0; j < 3; j++) {
         let hueSmall = random(360);
         let shade = color(hueSmall, 80, 70, 0.7);
         let radius = maxRadius * (1.0) * (1 - j * 0.2) * 0.9;
         circles.push(new SmallCircle(x, y, radius, shade));
       }
-
+      // Generate a hue for the central point of the cluster
       let hueCenter = random(360);
       circles.push(new CircleCenter(x, y, maxRadius * 0.2, color(hueCenter, 100, 50, 0.7)));
     }
@@ -47,6 +60,7 @@ function makeCircles() {
 }
 
 function windowResized() {
+  // Resize the canvas to fit the window
   resizeCanvas(windowWidth, windowHeight);
   makeCircles();
 }
@@ -58,14 +72,16 @@ function draw() {
   // Draw the circles
   for (let circle of circles) {
     if (circle instanceof BigCircle) {
+      // Update the rotation of the central circles
       circle.update();
+      // Update the scale of the central circles
       circle.updateScale(scaleUp);
     }
     circle.show();
   }
-
+  // Increment the frame counter
   frameCount++;
-
+  // If small circle animation is enabled, rotate start
   if (smallCirclesAnimated) {
     animateSmallCircles();
   }
@@ -73,6 +89,7 @@ function draw() {
 
 
 function animateSmallCircles() {
+  // Rotate the small circles
   for (let circleObject of circles) {
     if (circleObject instanceof SmallCircle) {
       circleObject.rotate();
@@ -84,6 +101,7 @@ function animateSmallCircles() {
 
 
 
+// Define the class for large central circles
 class BigCircle {
   constructor(x, y, radius, base) {
     this.pos = createVector(x, y);
@@ -91,12 +109,14 @@ class BigCircle {
     this.rotate = 0;
     this.radius = radius;
   }
-
+  // Update the rotation of the central circles
   update() {
-    this.rotate += 1; // Adjust the rotation speed for the BigCircle
+    // Adjust the rotation speed for the BigCircle
+    this.rotate += 1;
   }
-
+  // Display the central circle
   show() {
+    // Push and pop matrix for translation and rotation
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.rotate);
@@ -118,7 +138,7 @@ class BigCircle {
     }
     pop();
   }
-
+  // Update the scale of the central circle
   updateScale(scaleUp) {
     if (scaleUp) {
       this.radius += 0.5; // Scale up
@@ -129,7 +149,7 @@ class BigCircle {
       }
     } else {
       this.radius -= 1; // Scale down
-      if (this.radius <= bigCircleRadius / 1.5) {
+      if (this.radius <= bigCircleRadius / 1.2) {
         console.log(this.radius)
         console.log("SWAP TO POSITIVE");
         console.log(bigCircleRadius);
@@ -141,7 +161,7 @@ class BigCircle {
   }
 }
 
-
+// Define the class for small circles
 class SmallCircle {
   constructor(x, y, radius, base) {
     this.pos = createVector(x, y);
@@ -149,11 +169,11 @@ class SmallCircle {
     this.radius = radius;
     this.rotation = 0; // Corrected the variable name
   }
-
+  // Rotate the small circles
   rotate() { // Corrected the method name
     this.rotation += 1; // Adjust the rotation speed for the SmallCircle
   }
-
+  // Display the small circle
   show() {
     push();
     translate(this.pos.x, this.pos.y);
@@ -187,14 +207,14 @@ class SmallCircle {
 
 
 
-
+// Define the class for the central point of the circle cluster
 class CircleCenter {
   constructor(x, y, radius, base) {
     this.pos = createVector(x, y);
     this.base = base;
     this.radius = radius;
   }
-
+  // Display the central point of the cluster
   show() {
     fill(this.base);
     stroke(255);
